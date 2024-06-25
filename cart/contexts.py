d242173b35efbe7c8f0b0a1297adac71825c7729
Gabriel Eborime <cosmicanswer@gmail.com>
2024-06-25 06:11:44 +0000

    updated cart_contents context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+
+from products.models import Product
 
 def cart_contents(request):
     """ allows use of 'cart_contents' across the site"""
@@ -7,6 +10,17 @@ def cart_contents(request):
     cart_items = []
     grand_total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        grand_total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'cart_items': cart_items,
