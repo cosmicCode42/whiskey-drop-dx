9d7180c7180b9a64089b20ed76ad3ce164c2e586
Gabriel Eborime <cosmicanswer@gmail.com>
2024-06-25 02:59:47 +0000

    added ProductAdmin to tidy up admin

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'rating',
+    )
+
+    ordering = ('sku',)
+
+admin.site.register(Product, ProductAdmin)
