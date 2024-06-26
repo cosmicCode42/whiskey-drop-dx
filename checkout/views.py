54cb3596a187d5d5a3666da94a41a0dcce030f4c
Gabriel Eborime <cosmicanswer@gmail.com>
2024-06-26 05:00:36 +0000

    started setting up checkout with stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -19,6 +19,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PIvWJFtempKWvVq78NxcNOfQgatEAaRsr65Ktu1OSfxvzmYmPlUaPmjPq9AEYYq0LLRhDGA4uywoeLc8Mewgcka002aB7ODwO',
+        'client_secret': 'test client secret',
     }
     
     return render(request, template, context)
