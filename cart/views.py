16cf803d6c6a014cce4d8221b524be99643b6147
Gabriel Eborime <cosmicanswer@gmail.com>
2024-06-26 08:24:39 +0000

    added proper formatting and css for toasts

--- cart/views.py
+++ cart/views.py
@@ -14,14 +14,17 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ A view to add items to the cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}.')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your cart.')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -37,8 +40,10 @@ def adjust_cart(request, item_id):
     
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}.')
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart.')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -52,6 +57,7 @@ def remove_from_cart(request, item_id):
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart.')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
