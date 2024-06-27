a80420b8f5794044a099eee138494e427678d0e3
Gabriel Eborime <cosmicanswer@gmail.com>
2024-06-27 06:05:26 +0000

    started coding add_product functionality

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models.functions import Lower
 
 from .models import Product
+from .forms import ProductFrom
 
 # Create your views here.
 def product_detail(request, product_id):
@@ -15,3 +16,14 @@ def product_detail(request, product_id):
     }
     
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """Add a product to the store"""
+    form = ProductFrom()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
