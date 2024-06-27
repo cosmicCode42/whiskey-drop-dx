1309a90f9f7af830d6fcc41db5de5c0a39579522
Gabriel Eborime <cosmicanswer@gmail.com>
2024-06-27 11:22:27 +0000

    media fix

--- whiskey_drop_dx/settings.py
+++ whiskey_drop_dx/settings.py
@@ -90,7 +90,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.contrib.messages.context_processors.media',
+                'django.template.context_processors.media',
                 'products.contexts.products',
                 'cart.contexts.cart_contents',
             ],
