2b6d413030d8e33929ca6388e9681bd5c54e08ed
Gabriel Eborime <cosmicanswer@gmail.com>
2024-10-09 10:20:46 +0000

    added new models for home page - Feature, Quote, MonthFav

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Feature(models.Model):
+
+	icon = models.CharField(max_length=254)
+	name = models.CharField(max_length=254)
+	description = models.TextField()
+	is_new = models.BooleanField()
+
+class Quote(models.Model):
+    
+    image = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    quote = models.CharField(max_length=254)
+    
+class MonthFav(models.Model):
+    
+    name = models.CharField(max_length=254)
+    country_of_origin = models.CharField(max_length=254)
+    rank = models.DecimalField(max_digits=3, decimal_places=0)
