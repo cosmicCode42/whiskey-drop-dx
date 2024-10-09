591c9ecd8fc727c936415fd1b1c40f91c690ee0f
Gabriel Eborime <cosmicanswer@gmail.com>
2024-10-09 11:51:40 +0000

    edited new models and made migrations

--- home/models.py
+++ home/models.py
@@ -6,7 +6,8 @@ class Feature(models.Model):
 	icon = models.CharField(max_length=254)
 	name = models.CharField(max_length=254)
 	description = models.TextField()
-	is_new = models.BooleanField()
+	is_new = models.BooleanField(default=False)
+
 
 class Quote(models.Model):
     
@@ -14,8 +15,9 @@ class Quote(models.Model):
     name = models.CharField(max_length=254)
     quote = models.CharField(max_length=254)
     
-class MonthFav(models.Model):
+
+class Favourites(models.Model):
     
+    rank = models.DecimalField(max_digits=3, decimal_places=0)
     name = models.CharField(max_length=254)
     country_of_origin = models.CharField(max_length=254)
-    rank = models.DecimalField(max_digits=3, decimal_places=0)
