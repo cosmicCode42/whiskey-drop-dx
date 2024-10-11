f4eaa356f824d4e417fc35b8489da9f1abb5db86
Gabriel Eborime <cosmicanswer@gmail.com>
2024-10-11 12:20:15 +0000

    added new models to home admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Feature, Quote, Favourite
 
 # Register your models here.
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = (
+        'icon',
+        'name',
+        'description',
+        'is_new',
+    )
+
+
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = (
+        'image',
+        'name',
+        'quote',
+    )
+
+
+class FavouriteAdmin(admin.ModelAdmin):
+    list_display = (
+        'rank',
+        'name',
+        'country_of_origin',
+    )
+
+    ordering = ('rank',)
+
+
+admin.site.register(Feature, FeatureAdmin)
+admin.site.register(Quote, QuoteAdmin)
+admin.site.register(Favourite, FavouriteAdmin)
